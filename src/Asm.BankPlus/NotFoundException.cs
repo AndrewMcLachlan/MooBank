99cd12dec157569c4b5610f548de3bdbb3615033
Andrew McLachlan <amc_home@hotmail.com>
2019-08-26 08:00:38 +1000

    Added a not found exception to allow returning of 404.

---
+++ src/Asm.BankPlus/NotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asm.BankPlus
+{
+    public sealed class NotFoundException : ApplicationException
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
