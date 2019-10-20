af430bbf1d563d67c3d687b9c354de7552fda41c
Andrew McLachlan <amc_home@hotmail.com>
2019-10-20 17:25:14 +1000

    Prep work for deployment

--- src/Asm.BankPlus.Database/dbo/Tables/TransactionTag.sql
+++ src/Asm.BankPlus.Database/dbo/Tables/TransactionTag.sql
@@ -2,6 +2,6 @@
 (
 	[TransactionTagId] INT NOT NULL IDENTITY(1,1),
     [Name] NVARCHAR(50) NOT NULL,
-    [Deleted] BIT NOT NULL CONSTRAINT [DF_Deleted] DEFAULT 0,
+    [Deleted] BIT NOT NULL CONSTRAINT [DF_TransactionTag_Deleted] DEFAULT 0,
     CONSTRAINT [PK_TransactionTag] PRIMARY KEY CLUSTERED (TransactionTagId),
 )
