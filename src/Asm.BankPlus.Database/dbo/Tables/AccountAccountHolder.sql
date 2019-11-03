1f93394c9d39a2ba720cc9bac1e1b218374244af
Andrew McLachlan <amc_home@hotmail.com>
2019-11-03 21:42:44 +1000

    Added missing foreign keys.

--- src/Asm.BankPlus.Database/dbo/Tables/AccountAccountHolder.sql
+++ src/Asm.BankPlus.Database/dbo/Tables/AccountAccountHolder.sql
@@ -2,5 +2,7 @@
 (
 	AccountId UNIQUEIDENTIFIER NOT NULL,
     AccountHolderId UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT PK_AccountAccountHolder PRIMARY KEY (AccountId, AccountHolderId)
+    CONSTRAINT PK_AccountAccountHolder PRIMARY KEY (AccountId, AccountHolderId),
+    CONSTRAINT FK_AccountAccountHolder_Account FOREIGN KEY (AccountId) REFERENCES Account(AccountId),
+    CONSTRAINT FK_AccountAccountHolder_AccountHolder FOREIGN KEY (AccountHolderId) REFERENCES AccountHolder(AccountHolderId),
 )
