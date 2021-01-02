2e10413da3c886eee2df6bdb1ec860ef5775b1df
Andrew McLachlan <amc_home@hotmail.com>
2021-01-02 10:30:33 +1000

    Fixed up some casing discrepencies in SQL views.

--- src/Asm.BankPlus.Database/dbo/Views/TagAmounts.sql
+++ src/Asm.BankPlus.Database/dbo/Views/TagAmounts.sql
@@ -1,5 +1,5 @@
 ﻿CREATE VIEW [dbo].[TagAmounts] AS
-    SELECT t.AccountId, tt.tagId, tt.[Name], t.TransactionTypeId, SUM(t.Amount) as Total
+    SELECT t.AccountId, tt.TagId, tt.[Name], t.TransactionTypeId, SUM(t.Amount) as Total
     FROM TransactionsTags tt
-    INNER JOIN [Transaction] t ON tt.transactionId = t.TransactionId
+    INNER JOIN [Transaction] t ON tt.TransactionId = t.TransactionId
     GROUP BY t.AccountId, tt.TagId, tt.[Name], t.TransactionTypeId
--- src/Asm.BankPlus.Database/dbo/Views/TagAmountsByMonth.sql
+++ src/Asm.BankPlus.Database/dbo/Views/TagAmountsByMonth.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[TagAmountsByMonth] AS
-    SELECT t.AccountId, tt.tagId, tt.[Name], t.TransactionTypeId, ty.[Description], SUM(t.Amount) as Total, DATEPART(M, t.TransactionTime) as [Month], DATEPART(YEAR, t.TransactionTime) as [Year]
+    SELECT t.AccountId, tt.TagId, tt.[Name], t.TransactionTypeId, ty.[Description], SUM(t.Amount) as Total, DATEPART(M, t.TransactionTime) as [Month], DATEPART(YEAR, t.TransactionTime) as [Year]
     FROM TransactionsTags tt
-    INNER JOIN [Transaction] t ON tt.transactionId = t.TransactionId
+    INNER JOIN [Transaction] t ON tt.TransactionId = t.TransactionId
     INNER JOIN [TransactionType] ty ON t.TransactionTypeId = ty.TransactionTypeId
     GROUP BY t.AccountId, tt.TagId, tt.[Name], t.TransactionTypeId, ty.[Description], DATEPART(M, t.TransactionTime), DATEPART(YEAR, t.TransactionTime)
