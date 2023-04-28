d0ec7b3a86457ae474c81cb24e9a5a87c385077f
Andrew McLachlan <andy@mclachlan.family>
2023-04-28 21:49:20 +1000

    Fixed an issue calculating the number of months between two dates.

--- src/Asm.MooBank/Extensions/DateExtensions.cs
+++ src/Asm.MooBank/Extensions/DateExtensions.cs
@@ -2,7 +2,23 @@
 
 public static class DateExtensions
 {
-    public static int DifferenceInMonths(this DateOnly date, DateOnly other) => Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
+    public static int DifferenceInMonths(this DateOnly date, DateOnly other)
+    {
+        var months = Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
 
-    public static int DifferenceInMonths(this DateTime date, DateTime other) => Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
+        // Round part months up or down
+        return date.Day - other.Day < 0 ? months + 1 :
+               date.Day - other.Day > 0 ? months - 1 :
+               months;
+    }
+
+    public static int DifferenceInMonths(this DateTime date, DateTime other)
+    {
+        var months = Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
+
+        // Round part months up or down
+        return date.Day - other.Day < 0 ? months + 1 :
+               date.Day - other.Day > 0 ? months - 1 :
+               months;
+    }
 }
