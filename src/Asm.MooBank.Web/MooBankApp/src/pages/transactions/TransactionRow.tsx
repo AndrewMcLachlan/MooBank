cfe42756ae2e1885c1f52c1a08bf7ed0021b6629
Andrew McLachlan <andy@mclachlan.family>
2023-05-06 14:30:40 +1000

    Fixed formatting of normal transaction row date.

--- src/Asm.MooBank.Web/MooBankApp/src/pages/transactions/TransactionRow.tsx
+++ src/Asm.MooBank.Web/MooBankApp/src/pages/transactions/TransactionRow.tsx
@@ -23,7 +23,7 @@ export const TransactionRow: React.FC<TransactionRowProps> = (props) => {
         <>
             <TransactionDetails transaction={props.transaction} show={showDetails} onHide={() => setShowDetails(false)} onSave={onSave} />
             <tr className="clickable transaction-row" onClick={() => setShowDetails(true)} title={props.transaction.notes}>
-                <td>{format(parseISO(props.transaction.transactionTime), "yyyy-MM-dd")}</td>
+                <td>{format(parseISO(props.transaction.transactionTime), "dd/MM/yyyy")}</td>
                 <td className="description" colSpan={props.colspan}>{props.transaction.description}</td>
                 <td className="amount">{props.transaction.amount.toLocaleString("en-AU", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</td>
                 <TransactionTransactionTagPanel as="td" transaction={props.transaction} />
