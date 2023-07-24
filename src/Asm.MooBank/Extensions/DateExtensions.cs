aa015ef3cef8dedda378dc110152444f37ae3da5
Andrew McLachlan <amclachlan@willowinc.com>
2023-07-24 15:46:28 +1000

    NuGet package update.

--- src/Asm.MooBank/Extensions/DateExtensions.cs
+++ src/Asm.MooBank/Extensions/DateExtensions.cs
@@ -2,26 +2,6 @@
 
 public static class DateExtensions
 {
-    public static int DifferenceInMonths(this DateOnly date, DateOnly other)
-    {
-        var months = Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
-
-        // Round part months up or down
-        return date.Day - other.Day < 0 ? months + 1 :
-               date.Day - other.Day > 0 ? months - 1 :
-               months;
-    }
-
-    public static int DifferenceInMonths(this DateTime date, DateTime other)
-    {
-        var months = Math.Abs((date.Year - other.Year) * 12 + (date.Month - other.Month));
-
-        // Round part months up or down
-        return date.Day - other.Day < 0 ? months + 1 :
-               date.Day - other.Day > 0 ? months - 1 :
-               months;
-    }
-
     public static DateOnly ToStartOfMonth(this DateOnly date)
     {
         return new DateOnly(date.Year, date.Month, 1);
