d2cf36f69c7bc1432189f629c8e15520f7e12389
Andrew McLachlan <andy@mclachlan.family>
2023-11-06 22:55:29 +1000

    Tidied up reference data.

--- src/Asm.MooBank.Modules.ReferenceData/Models/ImporterType.cs
+++ src/Asm.MooBank.Modules.ReferenceData/Models/ImporterType.cs
@@ -1,53 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿namespace Asm.MooBank.Modules.ReferenceData.Models;
 
-namespace Asm.MooBank.Modules.ReferenceData.Models;
-
-public partial record ImporterType
+public sealed record ImporterType
 {
     public int Id { get; set; }
 
-    public string Type { get; set; }
+    public required string Type { get; set; }
 
-    public string Name { get; set; }
+    public required string Name { get; set; }
 }
 
-
-
-public partial record ImporterType
+public static class ImporterTypeExtensions
 {
-    public static explicit operator ImporterType(Domain.Entities.ReferenceData.ImporterType entity)
-    {
-        return new ImporterType
+    public static ImporterType ToModel(this Domain.Entities.ReferenceData.ImporterType entity) =>
+        new()
         {
             Id = entity.ImporterTypeId,
             Type = entity.Type,
             Name = entity.Name,
         };
-    }
 
-    public static explicit operator Domain.Entities.ReferenceData.ImporterType(ImporterType model)
-    {
-        return new Domain.Entities.ReferenceData.ImporterType
-        {
-            ImporterTypeId = model.Id,
-            Type = model.Type,
-            Name = model.Name,
-        };
-    }
-}
-
-public static class IEnumerableImporterTypeExtensions
-{
-    public static async Task<IEnumerable<ImporterType>> ToModelAsync(this IQueryable<Domain.Entities.ReferenceData.ImporterType> entities, CancellationToken cancellationToken = default) =>
-        await entities.Select(t => (ImporterType)t).ToArrayAsync(cancellationToken);
-
-    public static IEnumerable<ImporterType> ToModel(this IEnumerable<Domain.Entities.ReferenceData.ImporterType> entities)
-    {
-        return entities.Select(t => (ImporterType)t);
-    }
-
-    public static async Task<IEnumerable<ImporterType>> ToModelAsync(this Task<IEnumerable<Domain.Entities.ReferenceData.ImporterType>> entityTask, CancellationToken cancellationToken = default)
-    {
-        return (await entityTask.WaitAsync(cancellationToken)).Select(t => (ImporterType)t);
-    }
+    public static IQueryable<ImporterType> ToModel(this IQueryable<Domain.Entities.ReferenceData.ImporterType> query) =>
+        query.Select(t => t.ToModel());
 }
