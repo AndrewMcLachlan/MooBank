765fd2f1ce391f92ad16ce4edc11690b80f8065b
Andrew McLachlan <andy@mclachlan.family>
2024-02-20 07:33:11 +1000

    Fixed weird dashboard build error.

--- src/Asm.MooBank.Web.App/src/pages/dashboard/Dashboard.tsx
+++ src/Asm.MooBank.Web.App/src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-import { Dashboard as DashboardPage } from "@andrewmclachlan/mooapp";
+import { Dashboard as DashboardPage, DashboardProps } from "@andrewmclachlan/mooapp";
 import { InOutWidget } from "components/Dashboard";
 import { PositionWidget } from "components/Dashboard";
 import { BudgetWidget } from "components/Dashboard/Budget";
 
+const props: DashboardProps = {
+    title: "Home",
+}
+
 export const Dashboard: React.FC = () => (
-    <DashboardPage title="Home">
+    <DashboardPage {...props}>
         <InOutWidget />
         <BudgetWidget />
         <PositionWidget />
