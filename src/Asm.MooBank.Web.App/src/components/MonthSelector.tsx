5ac40b9d978844d004b34b01592aa7c6b0523ab1
Andrew McLachlan <andy@mclachlan.family>
2024-05-03 19:40:18 +1000

    Month Selector default props fix.

--- src/Asm.MooBank.Web.App/src/components/MonthSelector.tsx
+++ src/Asm.MooBank.Web.App/src/components/MonthSelector.tsx
@@ -14,8 +14,10 @@ export const MonthSelector: React.FC<MonthSelectorProps<any>> = ({ className, as
         props.onChange?.(newValue);
     }
 
+    const As = as;
+
     return (
-        <props.as className={classNames(className, "month-selector")}>
+        <As className={classNames(className, "month-selector")}>
             <input type="checkbox" id="jan" value={0} checked={isMonthSelected(value, 0)} onChange={onChange} />
             <input type="checkbox" id="feb" value={1} checked={isMonthSelected(value, 1)} onChange={onChange} />
             <input type="checkbox" id="mar" value={2} checked={isMonthSelected(value, 2)} onChange={onChange} />
@@ -29,7 +31,7 @@ export const MonthSelector: React.FC<MonthSelectorProps<any>> = ({ className, as
             <input type="checkbox" id="nov" value={10} checked={isMonthSelected(value, 10)} onChange={onChange} />
             <input type="checkbox" id="dec" value={11} checked={isMonthSelected(value, 11)} onChange={onChange} />
             <svg height="20" width="20" viewBox="0 0 20 20" focusable="false" className="clickable clear" onClick={() => setValue(0)}><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
-        </props.as>
+        </As>
     );
 }
 
