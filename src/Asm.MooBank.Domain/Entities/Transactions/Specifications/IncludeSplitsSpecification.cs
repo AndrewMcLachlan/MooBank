ce80111e283b7477f42751d6804a3230825032e9
Andrew McLachlan <AndrewMcLachlan@users.noreply.github.com>
2024-10-13 20:54:24 +1000

    Fixed an issue updating offset amount.

--- src/Asm.MooBank.Domain/Entities/Transactions/Specifications/IncludeSplitsSpecification.cs
+++ src/Asm.MooBank.Domain/Entities/Transactions/Specifications/IncludeSplitsSpecification.cs
@@ -4,5 +4,5 @@ namespace Asm.MooBank.Domain.Entities.Transactions.Specifications;
 public class IncludeSplitsSpecification : ISpecification<Transaction>
 {
     public IQueryable<Transaction> Apply(IQueryable<Transaction> query) =>
-        query.Include(t => t.Splits).ThenInclude(t => t.Tags).ThenInclude(t => t.Settings);
+        query.Include(t => t.Splits).ThenInclude(t => t.Tags).ThenInclude(t => t.Settings).Include(t => t.Splits).ThenInclude(t => t.OffsetBy);
 }
