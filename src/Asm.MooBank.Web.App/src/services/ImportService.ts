2e26cfd2d464d91a5000ec9bf898e5453d01388f
Andrew McLachlan <AndrewMcLachlan@users.noreply.github.com>
2024-11-02 14:08:59 +1000

    Invalidate account when importing transactions. Fixes #282.

--- src/Asm.MooBank.Web.App/src/services/ImportService.ts
+++ src/Asm.MooBank.Web.App/src/services/ImportService.ts
@@ -1,5 +1,16 @@
 import { useApiPostEmpty, useApiPostFile } from "@andrewmclachlan/mooapp";
+import { useQueryClient } from "@tanstack/react-query";
+import { accountsKey } from "./AccountService";
 
-export const useImportTransactions = () => useApiPostFile<{instrumentId: string, file: File}>((variables) => `api/instruments/${variables.instrumentId}/import`);
+export const useImportTransactions = () => {
 
-export const useReprocessTransactions = () => useApiPostEmpty<unknown, {instrumentId: string}>((variables) => `api/instruments/${variables.instrumentId}/import/reprocess`);
+    const client = useQueryClient();
+
+    return useApiPostFile<{ instrumentId: string, file: File }>((variables) => `api/instruments/${variables.instrumentId}/import`, {
+        onSuccess(data, variables, context) {
+            client.invalidateQueries({ queryKey: [accountsKey, variables.instrumentId] });
+        },
+    });
+}
+
+export const useReprocessTransactions = () => useApiPostEmpty<unknown, { instrumentId: string }>((variables) => `api/instruments/${variables.instrumentId}/import/reprocess`);
