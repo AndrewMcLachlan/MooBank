5a965ac998eee87b861875ca1b0d8d9ff4155a5b
Andrew McLachlan <andy@mclachlan.family>
2025-12-07 22:24:44 +1000

    Fixed security hole in Delete (close) virtual instrument.

--- src/Asm.MooBank.Modules.Instruments/Commands/VirtualInstruments/Delete.cs
+++ src/Asm.MooBank.Modules.Instruments/Commands/VirtualInstruments/Delete.cs
@@ -1,15 +1,19 @@
-﻿using Asm.MooBank.Commands;
+﻿using Asm.MooBank.Domain.Entities.Instrument.Specifications;
 using IInstrumentRepository = Asm.MooBank.Domain.Entities.Instrument.IInstrumentRepository;
 
 namespace Asm.MooBank.Modules.Instruments.Commands.VirtualInstruments;
 
-public record Delete(Guid InstrumentId, Guid VirtualAccountId) : ICommand;
+public record Delete(Guid InstrumentId, Guid VirtualInstrumentId) : ICommand;
 
 internal class DeleteHandler(IInstrumentRepository instrumentRepository, IUnitOfWork unitOfWork) : ICommandHandler<Delete>
 {
     public async ValueTask Handle(Delete command, CancellationToken cancellationToken)
     {
-        instrumentRepository.Delete(command.VirtualAccountId);
+        var instrument = await instrumentRepository.Get(command.InstrumentId, new VirtualAccountSpecification(), cancellationToken);
+
+        if (!instrument.VirtualInstruments.Any(a => a.Id == command.VirtualInstrumentId)) throw new NotFoundException();
+
+        instrumentRepository.Delete(command.VirtualInstrumentId);
 
         await unitOfWork.SaveChangesAsync(cancellationToken);
     }
