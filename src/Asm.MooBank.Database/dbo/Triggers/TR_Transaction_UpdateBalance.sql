3263bcf4434243b5148bb7971021eb890e05b5f9
Andrew McLachlan <andy@mclachlan.family>
2025-12-15 07:10:04 +1000

    Improvements to the trigger.

--- src/Asm.MooBank.Database/dbo/Triggers/TR_Transaction_UpdateBalance.sql
+++ src/Asm.MooBank.Database/dbo/Triggers/TR_Transaction_UpdateBalance.sql
@@ -5,24 +5,43 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    DECLARE @AffectedAccounts TABLE (AccountId UNIQUEIDENTIFIER);
+    DECLARE @BalanceChanges TABLE (AccountId UNIQUEIDENTIFIER PRIMARY KEY, NetChange DECIMAL(12, 4));
 
-    INSERT INTO @AffectedAccounts
-    SELECT AccountId FROM inserted WHERE AccountId IS NOT NULL
-    UNION
-    SELECT AccountId FROM deleted WHERE AccountId IS NOT NULL;
+    -- Calculate the net change to apply to the balance (Delta Update)
+    -- This is O(1) complexity regardless of account history size
+    INSERT INTO @BalanceChanges (AccountId, NetChange)
+    SELECT 
+        AccountId, 
+        SUM(AmountSigned)
+    FROM (
+        -- New values (Add to balance)
+        SELECT 
+            AccountId, 
+            CASE WHEN TransactionTypeId = 1 THEN Amount ELSE -ABS(Amount) END as AmountSigned
+        FROM inserted
+        WHERE AccountId IS NOT NULL
+        
+        UNION ALL
+        
+        -- Old values (Subtract from balance)
+        SELECT 
+            AccountId, 
+            -(CASE WHEN TransactionTypeId = 1 THEN Amount ELSE -ABS(Amount) END) as AmountSigned
+        FROM deleted
+        WHERE AccountId IS NOT NULL
+    ) d
+    GROUP BY AccountId;
 
+    -- Apply changes
     UPDATE ti
     SET 
-        Balance = ISNULL(t.Balance, 0),
-        LastTransaction = CAST(t.LastTransaction AS DATE)
+        Balance = ISNULL(ti.Balance, 0) + bc.NetChange,
+        -- Finding MAX on a Clustered Index (AccountId, TransactionTime) is extremely fast (Seek)
+        LastTransaction = (
+            SELECT MAX(TransactionTime) 
+            FROM [dbo].[Transaction] t 
+            WHERE t.AccountId = ti.InstrumentId
+        )
     FROM [dbo].[TransactionInstrument] ti
-    INNER JOIN @AffectedAccounts a ON ti.InstrumentId = a.AccountId
-    OUTER APPLY (
-        SELECT 
-            SUM(CASE WHEN TransactionTypeId = 1 THEN Amount ELSE -ABS(Amount) END) as Balance,
-            MAX(TransactionTime) as LastTransaction
-        FROM [dbo].[Transaction] t
-        WHERE t.AccountId = ti.InstrumentId
-    ) t;
+    INNER JOIN @BalanceChanges bc ON ti.InstrumentId = bc.AccountId;
 END
