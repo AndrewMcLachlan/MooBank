be3d1bb7a7f2a194f8b51a9258a83525092e6a0c
Andrew McLachlan <andy@mclachlan.family>
2025-12-14 12:57:07 +1000

    Improved performance of the TransactionInstrument table

--- src/Asm.MooBank.Database/dbo/Tables/TransactionInstrument.sql
+++ src/Asm.MooBank.Database/dbo/Tables/TransactionInstrument.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[TransactionInstrument]
 (
     [InstrumentId] UNIQUEIDENTIFIER NOT NULL,
-    [LastTransaction] AS dbo.LastTransaction([InstrumentId]),
-    [Balance] AS dbo.AccountBalance([InstrumentId]),
+    [LastTransaction] DATE NULL,
+    [Balance] DECIMAL(12, 4) NOT NULL CONSTRAINT DF_TransactionInstrument_Balance DEFAULT 0,
     CONSTRAINT PK_TransactionAccount PRIMARY KEY CLUSTERED ([InstrumentId]),
     CONSTRAINT FK_TransactionAccount_Account FOREIGN KEY ([InstrumentId]) REFERENCES [Instrument]([Id]),
 )
---
+++ src/Asm.MooBank.Database/dbo/Triggers/TR_Transaction_UpdateBalance.sql
@@ -0,0 +1,28 @@
+CREATE TRIGGER [dbo].[TR_Transaction_UpdateBalance]
+ON [dbo].[Transaction]
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @AffectedAccounts TABLE (AccountId UNIQUEIDENTIFIER);
+
+    INSERT INTO @AffectedAccounts
+    SELECT AccountId FROM inserted WHERE AccountId IS NOT NULL
+    UNION
+    SELECT AccountId FROM deleted WHERE AccountId IS NOT NULL;
+
+    UPDATE ti
+    SET 
+        Balance = ISNULL(t.Balance, 0),
+        LastTransaction = CAST(t.LastTransaction AS DATE)
+    FROM [dbo].[TransactionInstrument] ti
+    INNER JOIN @AffectedAccounts a ON ti.InstrumentId = a.AccountId
+    OUTER APPLY (
+        SELECT 
+            SUM(CASE WHEN TransactionTypeId = 1 THEN Amount ELSE -ABS(Amount) END) as Balance,
+            MAX(TransactionTime) as LastTransaction
+        FROM [dbo].[Transaction] t
+        WHERE t.AccountId = ti.InstrumentId
+    ) t;
+END
